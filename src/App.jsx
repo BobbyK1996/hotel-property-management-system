5a7508857418b7e75a953c59ad053a351bb46d17
BobbyK1996 <bobbykamal@hotmail.co.uk>
2024-04-22 23:36:07 +0100

    Learning basics of styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 8px;
+  background-color: purple;
+  color: white;
+  cursor: pointer;
+  margin: 20px;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  padding: 0.8rem 1.2rem;
+`;
+
 function App() {
-  return <div>test</div>;
+  return (
+    <div>
+      <H1>River&apos;s Edge Retreat</H1>
+      <Button onClick={() => alert('Check in')}>Check in</Button>
+      <Button onClick={() => alert('Check out')}>Check out</Button>
+
+      <Input type="number" placeholder="Number of guests" />
+    </div>
+  );
 }
 
 export default App;
