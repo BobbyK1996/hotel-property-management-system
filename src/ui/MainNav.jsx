bd79f9f4d841adeb2269e564814f90538bcee312
BobbyK1996 <bobbykamal@hotmail.co.uk>
2024-04-29 01:20:53 +0100

    Created the basics for a navbar and logo component. Created a low quality mock logo as a placeholder

---
+++ src/ui/MainNav.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+// const NavList = styled.ui`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 0.8rem;
+// `;
+
+function MainNav() {
+  return <nav>Nav</nav>;
+}
+
+export default MainNav;
