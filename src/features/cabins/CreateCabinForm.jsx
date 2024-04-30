aa7e89871e2305a7ae2eecf811ee084d928d1d07
BobbyK1996 <bobbykamal@hotmail.co.uk>
2024-04-30 14:44:36 +0100

    styled CreateCabinForm. Decided that this will eventually be a modal, so styling is still subject to change

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -4,9 +4,36 @@ import Form from '../../ui/Form';
 import Input from '../../ui/Input';
 import Button from '../../ui/Button';
 
-const FormRow = styled.div``;
+const FormRow = styled.div`
+  display: grid;
+  align-items: center;
+  grid-template-columns: 24rem 1fr 1.2fr;
+  gap: 2.4rem;
 
-const Label = styled.label``;
+  padding: 1.2rem 0;
+
+  &:first-child {
+    padding-top: 0;
+  }
+
+  &:last-child {
+    padding-bottom: 0;
+  }
+
+  &:not(:last-child) {
+    border-bottom: 1px solid var(--color-grey-100);
+  }
+
+  &:has(button) {
+    display: flex;
+    justify-content: flex-end;
+    gap: 1.2rem;
+  }
+`;
+
+const Label = styled.label`
+  font-weight: 500;
+`;
 
 function CreateCabinForm() {
   return (
