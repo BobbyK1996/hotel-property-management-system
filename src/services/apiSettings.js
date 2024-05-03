f6254a4dde22e08d7ea1581539671cff05ea040a
BobbyK1996 <bobbykamal@hotmail.co.uk>
2024-05-03 22:42:13 +0100

    Created an apiSettings file. Used supabase and apiCabins as reference to build getSettings and updateSettings functions. Since all settings will be stored on one row only, we will use an hardcoded id of one for the .eq function

---
+++ src/services/apiSettings.js
@@ -0,0 +1,28 @@
+import supabase from './supabase';
+
+export async function getSettings() {
+  const { data, error } = await supabase.from('settings').select('*');
+
+  if (error) {
+    console.error(error);
+    throw new Error('Settings could not be loaded');
+  }
+
+  return data;
+}
+
+export async function updateSettings(newSetting) {
+  //All settings are on one row only with an Id of 1, hence we only need to update Id=1
+  const { data, error } = await supabase
+    .from('settings')
+    .update(newSetting)
+    .eq('id', 1)
+    .select()
+    .single();
+
+  if (error) {
+    console.error(error);
+    throw new Error('Settings could not be updated');
+  }
+  return data;
+}
